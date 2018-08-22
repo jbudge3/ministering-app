92d0697ca899f54a4c7bce3e7811d15b576ed10a
Jake Budge <jbudge@bamboohr.com>
2018-08-22 09:38:20 -0600

    Connect to db, organize file

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,33 @@
+// Dependecy imports
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
+// Local utility imports
+import {getSecret} from './secrets';
+// Model imports
 
+
+// Initialize express/router
 const app = express();
 const router = express.Router();
 
+// API and DB setup
 const API_PORT = process.env.API_PORT || 3001;
+mongoose.connect(getSecret('dbUri'), {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+// GET routes
 router.get('/', (req, res) => {
 	res.json({message: 'Hello, world!'});
 });
 
+// Listen
 app.use('/api', router);
-
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
