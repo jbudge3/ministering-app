5a682219262d1510ea8896de8598e532b3579c39
Jake Budge <jbudge@bamboohr.com>
2018-08-24 17:15:12 -0600

    Clear out session data upon failed login attempt

--- backend/route-callbacks/login.js
+++ backend/route-callbacks/login.js
@@ -2,6 +2,8 @@ export function login(req, res, Users) {
 	const {username, password} = req.body;
 	Users.authenticate(username, password, (error, user) => {
 		if (error || !user) {
+			req.session.userId = null;
+			req.session.isAdmin = false;
 			return res.json({success: false, error});
 		} else {
 			req.session.userId = user._id;
