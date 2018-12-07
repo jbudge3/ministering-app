ab53667bd7786ba22df78b95097977df1e18a163
Jake Budge <jbudge@bamboohr.com>
2018-12-06 21:11:28 -0700

    fixes...hopefully

--- client/src/ajax/index.js
+++ client/src/ajax/index.js
@@ -24,12 +24,7 @@ export function login(username, password) {
 // GET all the stuff
 export function getAllMembers() {
 	return new Promise((resolve, reject) => {
-		fetch('/api/members', {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
+		fetch('/api/members')
 			.then(response => response.json())
 			.then((response) => {
 				if (response.success) {
